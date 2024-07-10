a0e575a45ed3852372e4131edc36379fcf907151
pawlowsfilip <pawlows.filip@gmail.com>
2024-07-10 12:48:30 +0200

    updated singly linked list

--- DS/Array/SinglyLinkedList.java
+++ DS/Array/SinglyLinkedList.java
@@ -41,6 +41,46 @@ public class SinglyLinkedList {
         head = newNode;
     }
 
+    public void lastInsert(int value) {
+        ListNode newNode = new ListNode(value);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        ListNode current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+
+    public void positionInsert(int value, int position) {
+        // create new node
+        ListNode newNode = new ListNode(value);
+
+        // if position is 1 change the
+        if (position == 1) {
+            newNode.next = head;
+            head = newNode;
+            // else loop to the position and change insert it
+        } else {
+            // create a counter and helper to reach the position
+            ListNode previous = head;
+            int counter = 1;
+
+            while (counter < position - 1) {
+                previous = previous.next;
+                counter++;
+            }
+
+            ListNode current = previous.next;
+            previous.next = newNode;
+            newNode.next = current;
+        }
+    }
+
 
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
@@ -50,10 +90,12 @@ public class SinglyLinkedList {
         ListNode third = new ListNode(8);
         ListNode fourth = new ListNode(11);
 
+
         // forming a chain
         sll.head.next = second; // 10 -> 1
         second.next = third;    // 10 -> 1 -> 8
         third.next = fourth;    // 10 -> 1 -> 8 -> null
+        sll.printList();
 
         // // print List
         // sll.printList();
@@ -61,10 +103,20 @@ public class SinglyLinkedList {
         // // count
         // System.out.println(sll.count());
 
-        // beginning insert
-        sll.beginningInsert(5);
-        sll.printList();
-        sll.beginningInsert(0);
+        // // beginning insert
+        // sll.beginningInsert(5);
+        // sll.printList();
+        // sll.beginningInsert(0);
+        // sll.printList();
+
+        // // last insertion
+        // sll.lastInsert(32);
+        // sll.printList();
+        // sll.lastInsert(365);
+        // sll.printList();
+
+        // position Insert
+        sll.positionInsert(26, 3);
         sll.printList();
     }
 }
